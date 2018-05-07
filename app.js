5e9d2b974152bf18d9af9e7aeb317ee46389e4a1
Anmol Arora <a6arora@ucsd.edu>
2018-05-06 17:46:50 -0700

    Creation of intitial skeleton

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var resource = require('./routes/resource');
+var about = require('./routes/about')
 // Example route
 // var user = require('./routes/user');
 
@@ -34,7 +36,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', index.view);
+app.get('/index', index.view);
+app.get('/resource', resource.view);
+app.get('/about', about.view);
 // Example route
 // app.get('/users', user.list);
 
